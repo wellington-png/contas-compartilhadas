a59c268540673a6e7cebff59db527a25b18315db
Wellington Santos <wellingtonasc38@gmail.com>
2024-08-31 14:01:00 -0300

    feat qrcode

--- lib/screens/home/widgets/new_room_button.dart
+++ lib/screens/home/widgets/new_room_button.dart
@@ -5,18 +5,40 @@ class NewRoomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: () => {},
-      icon: const Icon(Icons.add),
-      label: const Text('Novo Grupo'),
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.lightBlueAccent,
-        padding: const EdgeInsets.symmetric(vertical: 15),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton.icon(
+          onPressed: () {
+            // Ação para criar um novo grupo
+          },
+          icon: const Icon(Icons.add),
+          label: const Text('Novo Grupo'),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.lightBlueAccent,
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            minimumSize: const Size(160, 50),
+          ),
         ),
-        minimumSize: const Size.fromHeight(50),
-      ),
+        ElevatedButton.icon(
+          onPressed: () {
+            // Ação para escanear QR code
+          },
+          icon: const Icon(Icons.qr_code_scanner),
+          label: const Text('Escanear QR Code'),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.greenAccent,
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            minimumSize: const Size(160, 50),
+          ),
+        ),
+      ],
     );
   }
 }
